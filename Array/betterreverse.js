0c0d1b3a1d0e792446d0eb22d9aa0209d6ed33dd
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-11-28 21:07:10 +0530

    modified reverse again

--- Array/betterreverse.js
+++ Array/betterreverse.js
@@ -1,6 +1,6 @@
 const reverse= strings => {
     // check if string  is there and logic for that....
-    if(typeof(strings)== 'string'){
+    if(typeof(strings)=== 'string'){
         let reversedstring=""
         for(let i=strings.length-1; i>=0; i--){
             reversedstring+= strings[i]
@@ -8,17 +8,21 @@ const reverse= strings => {
         return reversedstring
     }
     // else if array  is there and logic for that....
-    else{
+    else if(typeof(strings)=== 'object' &&  strings instanceof Array){
         let reversedarray=[]
         for(let i=strings.length-1,j=0; i>=0; i--,j++){
             reversedarray[j] = strings[i]
         }
         return reversedarray
     }
-   // if(typeof(string))
+    else{
+        return ("Please Pass Correct Datatype!!")
+    }
 }
 
-console.log(reverse("abcs"))
-// output scba
-console.log(reverse([1,2,4]))
-// output [ 4, 2, 1 ]
+console.log(reverse("abcs xyzc"))
+// output czyx scba
+console.log(reverse([1,2,4,5,'a','b',10000000,11.5465,[1,4,6],6]))
+// output [ 6, [ 1, 4, 6 ], 11.5465, 10000000, 'b', 'a', 5, 4, 2, 1 ]
+console.log(reverse({0:1}))
+// Please Pass Correct Datatype!!
