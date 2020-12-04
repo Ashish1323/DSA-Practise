3476e4fb0171fa550c107b7e5819a4371e27825e
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-12-04 14:50:35 +0530

    longest substring bug

--- Strings/longest-substring-without-repeating-characters.js
+++ Strings/longest-substring-without-repeating-characters.js
@@ -9,7 +9,7 @@ var lengthOfLongestSubstring = function(s) {
         count=0
         j=map[s[i]]+1
         i=j;
-        map= new Map() 
+        map= {} 
     }
     
     count++ 
@@ -21,7 +21,42 @@ var lengthOfLongestSubstring = function(s) {
   return Math.max(...finalArray)
 };
 
-console.log(lengthOfLongestSubstring("abcabcbb"))
-console.log(lengthOfLongestSubstring("bbbbb"))
-console.log(lengthOfLongestSubstring("pwwkew"))
-console.log(lengthOfLongestSubstring("dvdf"))
+var lengthOfLongestSubstring1 = function(s) {
+  let count=0;
+  let finalArray=[]
+  let map={}
+  var j=0
+  let leftKey=0;
+  let RightKey=0;
+
+  for(let i=0; i<s.length ; i++){
+    if(Object.keys(map).find(ele=>ele==s[i]) && map[s[i]]>=leftKey){
+        RightKey=i
+        count=RightKey-leftKey
+        finalArray.push(count)
+        leftKey=map[s[i]]+1
+        count=0
+        i=leftKey
+        // j=map[s[i]]+1
+        // i=j;
+       // map= {} 
+    }
+    
+    count++ 
+    map[s[i]]=i
+    
+  }
+  finalArray.push(count)
+  console.log(finalArray) 
+
+  //console.log(finalArray)
+  return Math.max(...finalArray)
+};
+
+
+
+console.log(lengthOfLongestSubstring1("abcabcbb"))
+console.log(lengthOfLongestSubstring1("bbbbb"))
+console.log(lengthOfLongestSubstring1("pwwkew"))
+console.log(lengthOfLongestSubstring1("dvdf"))
+console.log(lengthOfLongestSubstring1("abcbdca"))
